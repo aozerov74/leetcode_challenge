b88eb680909e7a1b9f33ea779c96d9e00854cf79
Andrei Ozerov <andrey.ozerov@oracle.com>
2020-09-06 14:03:01 -0700

    [LeetCode Challenge] Initial task in the leetcode
    Easy: Count and say task

---
+++ src/leetcode/challenge/Challenge.java
@@ -0,0 +1,12 @@
+package leetcode.challenge;
+
+import leetcode.challenge.easy.CountAndSay;
+
+public class Challenge {
+    protected void test() {}
+    
+    public static final void main(String args[]) {
+        Challenge chl = new CountAndSay();
+        chl.test();
+    }
+}
